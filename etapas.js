d7e4d38e54ad149bbbaa37973a88560c27a73b6d
Cleiton da Silva Vasconcelos <cleitonvasconcelos_28@hotmail.com>
2021-01-20 11:29:11 -0300

    etapas da Urna Eletronica

---
+++ etapas.js
@@ -0,0 +1,54 @@
+let etapas = [
+    {
+       titulo: 'Vereador',
+       numeros: 5,
+       canditados: [
+           {
+              numero: '123456',
+              name: 'Fulano de tal',
+              partido: 'ABC',
+              fotos:[
+                    {url:'01.jpg', legenda:'Vereador',small: fase}
+                ]
+            },
+            {
+                numero: '789456',
+                name: 'Beltrano tal q',
+                partido: 'Ditador',
+                fotos:[
+                    {url: '03.jpg', legenda:'Vereador'}
+                ]
+            },
+
+
+
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+        {
+            numero: '1256',
+            name: 'Ciclano',
+            partido: 'Partido Partido',
+            vice: 'Clara',
+            fotos:[
+                {url: '04.jpg', legenda: 'Prefeito'},
+                {url: '05.jpg', legenda: 'Vice-Prefeito', small: true   }
+                ]
+        },
+        {
+            numero: '4578',
+            name: 'Aquamen',
+            partido: 'Partido Atlantida',
+            vice: 'Netuno',
+            fotos: [
+                {url: '06.jpg', legenda: 'Prefeito'},
+                {url: '01.jpg', legenda: 'Vide-Prefeito', small: true}
+                    ]
+        },
+
+    ]
+}
+];
